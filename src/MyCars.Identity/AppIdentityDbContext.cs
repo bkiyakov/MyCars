588593672477b99a22ecf9c23b08482254ee57ca
bkiyakov <bkiyakov@yandex.ru>
2020-12-18 13:06:00 +0300

    добавил проект identity

---
+++ src/MyCars.Identity/AppIdentityDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using MyCars.Identity.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCars.Identity
+{
+    public class AppIdentityDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, int, IdentityUserLogin<int>, IdentityUserRole<int>, IdentityUserClaim<int>>
+    {
+    }
+}
